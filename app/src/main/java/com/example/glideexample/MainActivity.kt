eb9e7b474dc3adc60267a3299aec0801a0e86201
student <s1068112@gm.pu.edu.tw>
2021-04-30 16:00:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/glideexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.glideexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val img: ImageView = findViewById(R.id.img)
+        GlideApp.with(this)
+                .load(R.drawable.earth)
+                .into(img)
+                
+
+
+    }
+}
